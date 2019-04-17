51a033bd999a840ea6ea54d86db7d52a7216a59a
Hitesh Kumar <hiteshkumar9@live.co.uk>
2019-04-18 00:36:41 +0100

    Vector interpolation function tested, cleaned and documented

--- source/interpolation.cpp
+++ source/interpolation.cpp
@@ -1,17 +1,29 @@
+#include "../includes/interpolation.h"
 
 
-using namespace std;
+void interpolate_vector(double* coarse_array, int coarse_size, double* fine_array, int fine_size) {
 
-
-void interpolate_vector() {
-	/* Interpolate vector from 2^n-1 elements to
-	2^(n+1)-1 elements.
+	/* Interpolate vector linearly from 2^n-1 elements to 2^(n+1)-1 elements.
 
 	Notes
 	-----
+	Performs simple linear interpolation, copying elements for 
+	odd indexes but otherwise taking a simple aritmetic average for
+	new elements generated in between.
 
 	Parameters
 	----------
+	double* coarse_array: pointer to array of doubles
+		Array of values for coarse version of vector
+
+	int coarse_size: Value greater than 0, a power of 2 + 1
+		The size of the coarse version of vector
+
+	double* fine_array: pointer to array of doubles
+		Array of values for coarse version of vector
+
+	int fine_size: Value greater than 0, a power of 2 + 1
+		The size of the fine version of vector
 
 	Returns
 	-------
@@ -19,5 +31,19 @@ void interpolate_vector() {
 
 	*/
 
+	// Special handling for end points
+	fine_array[0] = 0.5 * coarse_array[0];
+	fine_array[fine_size - 1] = 0.5 * coarse_array[coarse_size - 1];
+
+	// Copying across odd-indexed elements
+	for (int i = 0; i < coarse_size; i++) {
+		fine_array[(i * 2) + 1] = coarse_array[i];
+	}
+
+	// Handling all the newly generated points
+	for (int i = 2; i < fine_size - 2; i += 2) {
+		fine_array[i] = 0.5 * fine_array[i - 1]
+					  + 0.5 * fine_array[i + 1];
+	}
 
 }
