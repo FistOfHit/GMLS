a981abcbd45d482f2902d0c8c6ebdc4e411adebe
Hitesh Kumar <hiteshkumar9@live.co.uk>
2019-04-19 02:41:06 +0100

    added Skeleton functions for all IO routines

--- includes/IO_routines.h
+++ includes/IO_routines.h
@@ -1,6 +1,15 @@
 #pragma once
+#include <string>
 
 
-void read_grid();
+using namespace std;
 
-void write_grid();
+
+void read_matrix(double* matrix, int num_rows, int num_cols, string filename);
+void write_matrix(double* matrix, int num_rows, int num_cols, string filename);
+
+void read_vector(double* vector, int size, string filename);
+void write_vector(double* vector, int size, string filename);
+
+void print_matrix(double* matrix, int num_rows, int num_cols);
+void print_vector(double* vector, int size);
--- source/IO_routines.cpp
+++ source/IO_routines.cpp
@@ -1,18 +1,41 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+
+#include "../includes/IO_routines.h"
 
 
 using namespace std;
 
 
-void read_grid() {
+void read_matrix(double* matrix, int num_rows, int num_cols, string filename) {
+
+	
+
+}
 
 
+void write_matrix(double* matrix, int num_rows, int num_cols, string filename) {
 
 }
 
 
-void write_grid() {
-
-
-
+void read_vector(double* vector, int size, string filename) {
 
 }
+
+
+void write_vector(double* vector, int size, string filename) {
+
+}
+
+
+void print_matrix(double* matrix, int num_rows, int num_cols) {
+
+}
+
+
+void print_vector(double* vector, int size) {
+
+}
+
