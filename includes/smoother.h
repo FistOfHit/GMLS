d44766156b2b5dad03e51815341c3da5e3d1123d
Hitesh Kumar <hitesh.kumar15@imperial.ac.uk>
2019-04-20 02:36:26 +0100

    Wrote, cleaned and tested GS and SOR smoothers

---
+++ includes/smoother.h
@@ -0,0 +1,10 @@
+#pragma once
+
+
+void gs_smooth(double* lhs_matrix, int num_rows, int num_cols,
+			   double* solution_vector, double* rhs_vector, 
+			   double num_iterations);
+
+void sor_smooth(double* lhs_matrix, int num_rows, int num_cols,
+				double* solution_vector, double* rhs_vector,
+				double num_iterations, double omega);
