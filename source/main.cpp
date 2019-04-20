5e48bdad704044e40727785d9ad72c607a425ce6
Hitesh Kumar <hitesh.kumar15@imperial.ac.uk>
2019-04-20 02:37:26 +0100

    added smoother tests

--- source/main.cpp
+++ source/main.cpp
@@ -8,6 +8,8 @@
 
 // Custom test includes
 #include "../includes/interpolation_tests.h"
+#include "../includes/restriction_tests.h"
+#include "../includes/smoother_tests.h"
 
 
 using namespace std;
@@ -15,7 +17,10 @@ using namespace std;
 
 int main() {
 
+	// Running some tests
 	run_interpolation_tests();
+	run_restriction_tests();
+	run_smoother_tests();
 
 	system("pause");
 
