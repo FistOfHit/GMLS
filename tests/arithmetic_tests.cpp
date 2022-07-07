d12f39338b10a03fea2036b4fcf56559fa0e3c40
FistOfHit <21037774+FistOfHit@users.noreply.github.com>
2022-07-07 17:32:14 +0100

    Adding more arithmetic tests and fixing checks

--- tests/arithmetic_tests.cpp
+++ tests/arithmetic_tests.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <iostream>
+#include <math.h>
 #include <vector>
 #include "../includes/src_includes/arithmetic.h"
 #include "../includes/src_includes/printing.h"
@@ -10,7 +11,7 @@ using vector = std::vector<float>;
 
 
 void test_add() {
-    /*
+    /* Test vector addition at various grid depths.
     */
 
     // Create test vectors for adding and storing result
@@ -18,14 +19,31 @@ void test_add() {
     vector result = vector(9, 0);
     vector ideal_result = vector{2, 4, 6, 8, 10, 12, 14, 16, 18};
 
-    if (!std::equal(result.begin(), result.end(), ideal_result)) {
-        std::cout << "FAIL" << "\n";
+    // Test on multiple grid levels
+    int num_grids = 3;
 
-        std::cout << "Expected output: " << "\n";
-        print_vector(ideal_result);
-        std::cout << "Actual output:   " << "\n";
-        print_vector(result);
-        return;
+    float tolerance = 10e-6;
+	bool any_mismatch = false;
+    for (auto grid_depth = 0; grid_depth < num_grids; grid_depth++) {
+        
+        // Determine stride length across vector/matrix
+        int stride = std::pow(2, grid_depth);
+
+        // Perform addition at this grid depth
+        add(test, test, grid_depth, result);
+        
+        // Check that addition was done correctly.
+        for (auto i = 0; i < result.size(); i += stride) {
+            if (abs(result[i] - ideal_result[i]) > tolerance) {
+                std::cout << "FAIL" << "\n";
+
+                std::cout << "Expected output: " << "\n";
+                print_vector(ideal_result);
+                std::cout << "Actual output:   " << "\n";
+                print_vector(result);
+                return;
+            }
+        }
     }
 
     std::cout << "PASS" << "\n";
@@ -36,7 +54,104 @@ void test_add() {
 }
 
 
-void test_subtract();
+void test_subtract() {
+    /* Test vector subtraction at various grid depths.
+    */
+
+    // Create test vectors for adding and storing result
+    vector test_a = vector{1, 2, 3, 4, 5, 6, 7, 8, 9};
+    vector test_b = vector{9, 8, 7, 6, 5, 4, 3, 2, 1};
+    vector result = vector(9, 0);
+    vector ideal_result = vector{-8, -6, -4, -2, 0, 2, 4, 6, 8};
+
+    // Test on multiple grid levels
+    int num_grids = 3;
+
+    float tolerance = 10e-6;
+	bool any_mismatch = false;
+    for (auto grid_depth = 0; grid_depth < num_grids; grid_depth++) {
+        
+        // Determine stride length across vector/matrix
+        int stride = std::pow(2, grid_depth);
+
+        // Perform addition at this grid depth
+        subtract(test_a, test_b, grid_depth, result);
+        
+        // Check that addition was done correctly.
+        for (auto i = 0; i < result.size(); i += stride) {
+            if (abs(result[i] - ideal_result[i]) > tolerance) {
+                std::cout << "FAIL" << "\n";
+
+                std::cout << "Expected output: " << "\n";
+                print_vector(ideal_result);
+                std::cout << "Actual output:   " << "\n";
+                print_vector(result);
+                return;
+            }
+        }
+    }
+
+    std::cout << "PASS" << "\n";
+
+    test_a = vector();
+    test_b = vector();
+    result = vector();
+    ideal_result = vector();
+}
 
 
-void test_multiply();
+void test_multiply() {
+    /* Test vector subtraction at various grid depths.
+    */
+
+    // Create test vectors for adding and storing result
+    vector test_matrix = vector{
+        1, 2, 3, 4, 5, 6, 7, 8, 9,
+        1, 2, 3, 4, 5, 6, 7, 8, 9,
+        1, 2, 3, 4, 5, 6, 7, 8, 9,
+        1, 2, 3, 4, 5, 6, 7, 8, 9,
+        1, 2, 3, 4, 5, 6, 7, 8, 9,
+        1, 2, 3, 4, 5, 6, 7, 8, 9,
+        1, 2, 3, 4, 5, 6, 7, 8, 9,
+        1, 2, 3, 4, 5, 6, 7, 8, 9,
+        1, 2, 3, 4, 5, 6, 7, 8, 9,
+    };
+    vector test_vector = vector{1, 2, 3, 4, 5, 6, 7, 8, 9};
+    vector result = vector(9, 0);
+
+    vector ideal_result = vector{385, 385, 385, 385, 385, 385, 385, 385, 385};
+
+    // Test on multiple grid levels
+    int num_grids = 3;
+
+    float tolerance = 10e-6;
+	bool any_mismatch = false;
+    for (auto grid_depth = 0; grid_depth < num_grids; grid_depth++) {
+        
+        // Determine stride length across vector/matrix
+        int stride = std::pow(2, grid_depth);
+
+        // Perform addition at this grid depth
+        subtract(test, test, grid_depth, result);
+        
+        // Check that addition was done correctly.
+        for (auto i = 0; i < result.size(); i += stride) {
+            if (abs(result[i] - ideal_result[i]) > tolerance) {
+                std::cout << "FAIL" << "\n";
+
+                std::cout << "Expected output: " << "\n";
+                print_vector(ideal_result);
+                std::cout << "Actual output:   " << "\n";
+                print_vector(result);
+                return;
+            }
+        }
+    }
+
+    std::cout << "PASS" << "\n";
+
+    test_a = vector();
+    test_b = vector();
+    result = vector();
+    ideal_result = vector();
+}
